e3baa6999d029557035d5d221aa47a7ed7b0c120
yumaon <110388364+yumaon@users.noreply.github.com>
2025-08-04 21:05:35 +0900

    サイドバーにoptionsを配置

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,5 +1,14 @@
 import React from 'react'
 import TwitterIcon from '@mui/icons-material/Twitter';
+import SidebarOption from './SidebarOption';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import Person2OutlinedIcon from '@mui/icons-material/Person2Outlined';
+import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 
 function Sidebar() {
   return (
@@ -8,7 +17,14 @@ function Sidebar() {
       <TwitterIcon className='sidebar_twitterIcon'/>
 
       {/* SidebarOption */}
-
+      <SidebarOption text="ホーム" Icon={HomeOutlinedIcon}/>
+      <SidebarOption text="話題を検索" Icon={SearchIcon}/>
+      <SidebarOption text="通知" Icon={NotificationsOutlinedIcon}/>
+      <SidebarOption text="メッセージ" Icon={EmailOutlinedIcon}/>
+      <SidebarOption text="ブックマーク" Icon={BookmarkBorderOutlinedIcon}/>
+      <SidebarOption text="リスト" Icon={FormatListBulletedIcon}/>
+      <SidebarOption text="プロフィール" Icon={Person2OutlinedIcon}/>
+      <SidebarOption text="もっとみる" Icon={MoreHorizOutlinedIcon}/>
       {/* ツイートボタン */}
     </div>
   )
---
+++ src/components/sidebar/SidebarOption.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SidebarOption({text, Icon}) {
+  return (
+    <div>
+      <Icon />
+      <h2>{text}</h2>
+    </div>
+  )
+}
+
+export default SidebarOption
